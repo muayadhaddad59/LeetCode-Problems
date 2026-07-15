3c562c74d5dc8f4a9127ea04ef0531d28bbeb495
Muayad Haddad <74570275+muayadhaddad59@users.noreply.github.com>
2026-07-14 23:29:20 -0400

    Initial Commit

---
+++ LeetCode-Problems/LeetCode_ProblemsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeetCode_ProblemsApp.swift
+//  LeetCode-Problems
+//
+//  Created by Macbook on 2026-07-14.
+//
+
+import SwiftUI
+
+@main
+struct LeetCode_ProblemsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
